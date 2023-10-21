b0f0995436c3b16ff653257f291baaf19cc849e7
Chagi <chagimgl@gmail.com>
2023-10-21 23:00:42 +0800

    jump game done

---
+++ top-interview-150/009-jump-game/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # Take curr variable to keep the current maximum jump...
+        curr = nums[0]
+        # Traverse all the elements through loop...
+        for i in range(1,len(nums)):
+            # If the current index 'i' is less than current maximum jump 'curr'...
+            # It means there is no way to jump to current index...
+            # so we should return false...
+            if curr == 0:
+                return False
+            curr -= 1
+            # Update the current maximum jump...
+            curr = max(curr, nums[i])       # It’s possible to reach the end of the array...
+        return True
+            
+        
+solution = Solution()
+
+nums = [8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5]
+solution.canJump(nums)
