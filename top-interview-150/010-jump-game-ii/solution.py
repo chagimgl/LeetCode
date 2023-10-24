243fa28e6fdd2aea00b3546741d6dd3d3efa1017
Chandmani-Erdene Tsend-Ayush <chagimgl@gmail.com>
2023-10-24 23:25:55 +0800

    problem 10 solved using bfs

---
+++ top-interview-150/010-jump-game-ii/solution.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        ans = 0
+        end = 0
+        farthest = 0
+
+        # Implicit BFS
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            if farthest >= len(nums) - 1:
+                ans += 1
+                break
+            if i == end:      # Visited all the items on the current level
+                ans += 1        # Increment the level
+                end = farthest  # Make the queue size for the next level
+        
+        return ans
+
+        
+
+solution = Solution()
+
+nums = [2,3,1,1,4]
+nums = [1,2,1,1,1]
+solution.jump(nums)
